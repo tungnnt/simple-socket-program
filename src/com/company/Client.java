88ac1cd8bd2d111c8b64ce0189124e7f9bda46db
tungnnt <17021353@vnu.edu.vn>
2020-03-22 13:44:18 -0400

    ok

---
+++ src/com/company/Client.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+// class mo ta client
+public class Client {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Client start...");
+
+        // ket noi den server localhost:3009
+        String serverHost = "localhost";
+        Socket socketOfClient = new Socket(serverHost, 3009);;
+        BufferedWriter clientSender = new BufferedWriter(new OutputStreamWriter(socketOfClient.getOutputStream()));
+        BufferedReader clientReceiver = new BufferedReader(new InputStreamReader(socketOfClient.getInputStream()));
+
+        try {
+            // nhan mssv tu nguoi dung nhap
+            Scanner in = new Scanner(System.in);
+            System.out.print("Client says enter studentId: ");
+            String stdID = in.nextLine();
+
+            // gui du lieu ma nguoi dung nhap den server
+            clientSender.write(stdID);
+            clientSender.newLine();
+            clientSender.flush();
+            System.out.println("Client -> Server: " +stdID);
+
+            // lay du lieu tu server gui ve
+            String responseOfServer;
+            if ((responseOfServer = clientReceiver.readLine()) != null)
+                System.out.println("Server -> Client: " + responseOfServer);
+
+            // client gui yeu cau dong ket noi
+            clientSender.write("End connection");
+            clientSender.newLine();
+            clientSender.flush();
+            if (!stdID.equals("End connection"))
+                System.out.println("Client -> Server: End connection");
+            System.out.println("Status: Disconnect to server");
+            clientSender.close();
+            clientReceiver.close();
+            socketOfClient.close();
+
+        } catch (UnknownHostException e) {
+            System.err.println("Host:port not found" + e);
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
+
+}
---
+++ src/com/company/Server.java
@@ -0,0 +1,129 @@
+package com.company;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+// class Student dai dien cho doi tuong sinh vien bao gom ten, mssv, lop, sdt, email, dia chi
+class Student {
+    // cac thuoc tinh cua sinh vien
+    private String studentId;
+    private String name;
+    private String classroom;
+    private String phone;
+    private String email;
+    private String address;
+
+    // ham khoi tao 1 doi tuong sinh vien
+    public Student(String studentId, String name, String classroom, String phone, String email, String address) {
+        this.studentId = studentId;
+        this.name = name;
+        this.classroom = classroom;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    // getter
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    // ham in thong tin sinh vien
+    @Override
+    public String toString() {
+        return "Student info: [Name: " + this.getName() + ", " +
+                "studentID: " + this.getStudentId() + "," +
+                "class: " + this.getClassroom() + "," +
+                "phone: " + this.getPhone() + ", " +
+                "email: " + this.getEmail() + ", " +
+                "address: " + this.getAddress() + "]";
+    }
+}
+
+// class mo ta Server
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Server start...");
+        // tao socket cua server va cho ket noi tu client
+        ServerSocket listener = new ServerSocket(3009);
+
+        System.out.println("Server accepts connection from client...");
+        // chap nhan ket noi tu client
+        Socket socketOfServer = listener.accept();;
+
+        // doc du lieu tu client, gui du lieu den client
+        BufferedReader serverReceiver = new BufferedReader(new InputStreamReader(socketOfServer.getInputStream()));
+        BufferedWriter serverSender = new BufferedWriter(new OutputStreamWriter(socketOfServer.getOutputStream()));
+
+        // mang chua cac doi tuong student
+        ArrayList<Student> students = new ArrayList<Student>();
+        Student aStudent = new Student("17021353", "Nguyen Ngoc Thanh Tung", "K62CACLC1", "0392479119", "cloneoftung@gmail.com", "Hai Duong");
+        students.add(aStudent);
+        aStudent = new Student("17021252", "Nguyen Huu Hoa", "K62CACLC1", "0392479118", "hoann@gmail.com", "Ha noi");
+        students.add(aStudent);
+        aStudent = new Student("17021050", "Nguyen Hop Quang", "K62CACLC1", "0392479118", "hoann@gmail.com", "Ha noi");
+        students.add(aStudent);
+
+        // doc du lieu tu client gui den
+        String line;
+        while (true) {
+            line = serverReceiver.readLine();
+            System.out.println("Server received: " + line);
+
+            // kiem tra xem clien co muon dong ket noi khong
+            if (line.equals("End connection")) {
+                serverSender.write("Accept end connection");
+                serverSender.newLine();
+                serverSender.flush();
+
+                // tat server
+                serverReceiver.close();
+                serverSender.close();
+                socketOfServer.close();
+                break;
+            }
+
+            // kiem tra xem co sinh vien co mssv can tim khong
+            boolean hasStudent = false;
+            for (Student student : students) {
+                if (student.getStudentId().equals(line)) {
+                    serverSender.write(student.toString());
+                    serverSender.newLine();
+                    serverSender.flush();
+                    hasStudent = true;
+                    break;
+                }
+            }
+
+            if (hasStudent == false){
+                serverSender.write("Student not available");
+                serverSender.newLine();
+                serverSender.flush();
+            }
+        }
+        System.out.println("Status: Sever stopped");
+    }
+}
